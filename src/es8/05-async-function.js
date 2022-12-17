6f74e5c566dc2273e3043dd7db6de13f7e518c01
CarlosMarioToro <111100510+CarlosMarioToro@users.noreply.github.com>
2022-12-17 17:35:36 -0500

    clase 21

---
+++ src/es8/05-async-function.js
@@ -0,0 +1,23 @@
+const fnAsync = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+            ? setTimeout(() => resolve('AsynC!!'), 2000)
+            : reject(new Error('Error!'));
+    });
+}
+
+// const anotherFn = async () => {
+//     const something = await fnAsync();
+//     console.log(something);
+//     console.log('Hola!');
+// }
+
+const anotherFn = () => {
+    const something = fnAsync();
+    console.log(something);
+    console.log('Hola!');
+}
+
+console.log('Before');
+anotherFn();
+console.log('After');
